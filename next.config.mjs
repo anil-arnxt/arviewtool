af5560932c23892c942c4bef0aa48e9739e4d5d5
anil-arnxt <anil.samal@arnxt.com>
2024-06-18 13:06:23 +0530

    new build

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         domains: ['codepencdn.unlimited3d.com', 'arnxtdealerpageproducts.s3.ap-south-1.amazonaws.com/'],
     },
